b43b05052e732b6998c6f7b6743678793190811f
Sonya <ellie_greys@mail.ru>
2024-06-02 20:38:01 +0300

    CustomEntry

---
+++ MuzApp/MuzApp.Android/CustomEntryRenderAndroid.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MuzApp;
+using MuzApp.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly : ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderAndroid))]
+namespace MuzApp.Droid
+{
+    public class CustomEntryRenderAndroid : EntryRenderer
+    {
+        public CustomEntryRenderAndroid(Context context) : base(context) { }
+        CustomEntry customEntry;
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(e.OldElement == null)
+            {
+                customEntry = (CustomEntry)e.NewElement;
+                var gradientDrawable = new GradientDrawable();
+
+                gradientDrawable.SetCornerRadius(customEntry.EntryCornerRadius);
+                gradientDrawable.SetStroke(2, customEntry.EntryBorderColor.ToAndroid());
+
+                Control.SetBackground(gradientDrawable);
+            }
+        }
+    }
+
+    
+}
---
+++ MuzApp/MuzApp/CustomEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MuzApp
+{
+    public class CustomEntry : Entry
+    {
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create("CornerRadius", typeof(int), typeof(CustomEntry), 0);
+        public int EntryCornerRadius
+        {
+            get {  return (int)GetValue(CornerRadiusProperty);}
+            set { SetValue(CornerRadiusProperty, value);}
+
+        }
+
+        public static readonly BindableProperty BorderColorProperty = BindableProperty.Create("BorderThickness", typeof(Color), typeof(CustomEntry), Color.Yellow);
+        public Color EntryBorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+    }
+}
